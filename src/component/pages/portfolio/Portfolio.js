501ef03b97ab1b7d62be4a2885eee4e54c5b9414
bikramkawan <bikramkawan@gmail.com>
2017-07-13 23:09:54 +0200

    add animation on portfolio images hover

--- src/component/pages/portfolio/Portfolio.js
+++ src/component/pages/portfolio/Portfolio.js
@@ -7,23 +7,71 @@ import wheels from './img/wheels.png';
 import portfoliobg from './img/portfoliobg.jpg'
 import ReactCSSTransitionGroup  from "react-addons-css-transition-group";
 import {Link} from 'react-router-dom'
+
+
+export const url = {
+
+    nu: 'http://www.nepalunderground.com/',
+    revamp: 'http://www.revampwear.com/',
+    wheels: 'https://thewheels.com.np/',
+    riderz: 'http://www.riderznepal.com'
+}
+
+const WebIframe = ({address})=> {
+    return (
+        <iframe className="portfolioURL animated fadeInRight" src={address} frameBorder="0" allowFullScreen>
+        </iframe>
+    )
+}
+
 class Portfolio extends Component {
+    constructor() {
+        super();
+        this.state = {
+            onMouse: false,
+            url: null
+        };
+    }
+
+
+    zoomIn = ({target:{alt}}) => {
+
+        this.setState({onMouse: true, url: alt});
+
+    }
+    zoomOut = () => {
+        this.setState({onMouse: false, url: null});
+    }
+
+
     render() {
-
         return (
-
-            <div style={{display: 'flex',height:'100%'}}>
+            <div style={{display: 'flex', height: '100%'}}>
                 <div className="portfolio">
-                    <img src={nu} alt={"nu"} className="animated jello"/>
-                    <img src={revamp} alt={"revamp"} className="animated jello"/>
-                    <img src={riderz} alt={"riderz"} className="animated jello"/>
-                    <img src={wheels} alt={"wheels"} className="animated jello"/>
+                    <img src={nu} alt={"nu"} className="animated jello"
+                         onMouseOver={this.zoomIn}
+                         onMouseLeave={this.zoomOut}/>
+                    <img src={revamp} alt={"revamp"} className="animated jello"
+                         onMouseOver={this.zoomIn}
+                         onMouseLeave={this.zoomOut}/>
+                    <img src={riderz} alt={"riderz"} className="animated jello" onMouseOver={this.zoomIn}
+                         onMouseLeave={this.zoomOut}/>
+                    <img src={wheels} alt={"wheels"} className="animated jello" onMouseOver={this.zoomIn}
+                         onMouseLeave={this.zoomOut}/>
                 </div>
-                <div style={{flexGrow: '1', width: '100%'}}>
-                    <img style={{width: '100%', height: '100%'}} src={portfoliobg} alt={"nu"}/>
+                <div style={{
+                    flexGrow: '1',
+                    width: '100%',
+                    display: 'flex',
+                    backgroundImage: `url(${portfoliobg})`,
+                    backgroundSize: 'cover'
+
+                }}>
+
+                    {this.state.onMouse ? <WebIframe address={url[this.state.url]}/> : ''}
                 </div>
 
-                </div>
+            </div>
 
         );
     }
