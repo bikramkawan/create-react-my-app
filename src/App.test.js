ea8a5030b650873b40caf2d2a95497180bcb4841
bikramkawan <bikramkawan@gmail.com>
2017-07-19 20:40:49 +0200

    fix the app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {HashRouter} from 'react-router-dom'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<HashRouter>
+        <App/>
+    </HashRouter>, div);
 });
