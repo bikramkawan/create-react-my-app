548117e05d6ef26d61a02f952e7d247167253066
bkawan <bikramkawan@gmail.com>
2017-07-21 17:14:01 +0200

    fix travis error

--- src/mobileOSVoting/MobileOsVoting.js
+++ src/mobileOSVoting/MobileOsVoting.js
@@ -2,15 +2,17 @@
  * Created by bikramkawan on 7/17/17.
  */
 import React, {Component} from 'react';
-import {createStore} from 'redux';
+
 import myApp from './reducers'
 import App from './component/App'
 import VotingResults from './component/VotingResults'
-let store = createStore(myApp, {
-    ios : 0,
-    android: 0,
-    windows: 0
-}, window.devToolsExtension())
+import { createStore, compose } from 'redux';
+const enhancer = compose(
+    window.devToolsExtension ? window.devToolsExtension({
+        name: 'MyApp', actionsBlacklist: ['REDUX_STORAGE_SAVE']
+    }) : noop => noop
+);
+let store = createStore(myApp, enhancer)
 
 
 class MobileOsVoting extends Component {
