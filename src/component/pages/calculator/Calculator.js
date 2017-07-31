40fd9f44dd6efee72fdb2d207b4012de5aac9fe9
bkawan <bikramkawan@gmail.com>
2017-07-31 18:46:43 +0200

    add event onclick

--- src/component/pages/calculator/Calculator.js
+++ src/component/pages/calculator/Calculator.js
@@ -2,36 +2,54 @@
  * Created by bikramkawan on 7/31/17.
  */
 import React, {Component} from 'react';
+import * as $ from 'jquery'
+
+
 
 
 class Calculator extends Component {
 
+    constructor(){
+        super();
+        this.state = {
+            textVal:''
+
+        }
+
+    }
+
+
+    onClick = ({target:{innerText}})=>{
+         this.setState({textVal:this.state.textVal.concat(innerText)});
+         $('.result').text(this.state.textVal.concat(innerText))
+
+    }
+
 
     render() {
-
-        return(
+        return (
             <div className="calcContainer">
                 <div className="buttonsContainer">
                     <div className="mybutton result">0</div>
-                    <div className="mybutton number resetBtn">AC</div>
-                    <div className="mybutton number ">+/-</div>
-                    <div className="mybutton number">%</div>
-                    <div className="mybutton operator">/</div>
-                    <div className="mybutton number">7</div>
-                    <div className="mybutton number">8</div>
-                    <div className="mybutton number">9</div>
-                    <div className="mybutton operator">*</div>
-                    <div className="mybutton number">4</div>
-                    <div className="mybutton number">5</div>
-                    <div className="mybutton number">6</div>
-                    <div className="mybutton operator">-</div>
-                    <div className="mybutton number">1</div>
-                    <div className="mybutton number">2</div>
-                    <div className="mybutton number">3</div>
-                    <div className="mybutton operator">+</div>
-                    <div className="mybutton number zero-btn">0</div>
-                    <div className="mybutton number dot">.</div>
-                    <div className="mybutton  operator">=</div>
+                    <div className="mybutton number resetBtn " onClick={this.onClick}>AC</div>
+                    <div className="mybutton number " onClick={this.onClick}>+/-</div>
+                    <div className="mybutton number" onClick={this.onClick}>%</div>
+                    <div className="mybutton operator" onClick={this.onClick}>/</div>
+                    <div className="mybutton number" onClick={this.onClick}>7</div>
+                    <div className="mybutton number" onClick={this.onClick}>8</div>
+                    <div className="mybutton number" onClick={this.onClick}>9</div>
+                    <div className="mybutton operator" onClick={this.onClick}>*</div>
+                    <div className="mybutton number" onClick={this.onClick}>4</div>
+                    <div className="mybutton number" onClick={this.onClick}>5</div>
+                    <div className="mybutton number" onClick={this.onClick}>6</div>
+                    <div className="mybutton operator" onClick={this.onClick}>-</div>
+                    <div className="mybutton number" onClick={this.onClick}>1</div>
+                    <div className="mybutton number" onClick={this.onClick}>2</div>
+                    <div className="mybutton number" onClick={this.onClick}>3</div>
+                    <div className="mybutton operator" onClick={this.onClick}>+</div>
+                    <div className="mybutton number zero-btn" onClick={this.onClick}>0</div>
+                    <div className="mybutton number dot" onClick={this.onClick}>.</div>
+                    <div className="mybutton  operator" onClick={this.onClick}>=</div>
                 </div>
             </div>
         )
@@ -40,4 +58,5 @@ class Calculator extends Component {
 
 
 }
-export  default Calculator
+
+export default Calculator
